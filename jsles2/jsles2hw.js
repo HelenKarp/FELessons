0d3abefb8b14dc57a42153537536c7ec876cde7e
Alona Karpenko <alona.karpenko44@gmail.com>
2022-11-04 18:45:49 +0200

    jshw2

---
+++ jsles2/jsles2hw.js
@@ -0,0 +1,25 @@
+let azerotwo = 0.2;
+    bzeroone = 0.1;
+    one = "1";
+    two = 2;
+console.log((azerotwo * 10 + 10 * bzeroone)/10);
+let result = alert ((two * one) + (one * one));
+let memory = prompt ("How many GB do you want?");
+let Result = alert (` OMG! It's equal to ${((memory * 1000)/820).toFixed(2)} times for 820Mb)`);
+let money = prompt ("How much money do you have?");
+let price = prompt ("How much does the chocobar cost?");
+let risult = (money/price);
+let risalt = (Math.floor(money/price));
+let change = ((risult - risalt) * price);
+let final = alert (` WOW! you can buy ${risalt} chokobar(s) and have ${change.toFixed(2)} gryvna(s) as a change!`);
+let number1 = prompt ("Put the xxx number");
+    alert(number1.split("").reverse().join(""));
+    sum = prompt ("Put any amount of money");
+    devidents = ((sum / 20)/6);
+    Risult = alert (`Congrats! you'll receive ${devidents.toFixed(2)} grivnas as adevidents!`)
+   
+
+  
+   
+
+
