4c55f0d9dbbed26924ded45cb483f45d067ab362
Alona Karpenko <alona.karpenko44@gmail.com>
2023-08-01 21:48:19 +0100

    new

---
+++ reactappmycola/js-and-react/src/App.js
@@ -0,0 +1,13 @@
+const age = 35;
+const movie = `favouriteMovie ${age}`;
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+     movie: {movie};
+    </div>
+  );
+}
+
+export default App;
