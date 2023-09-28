ab39d039eadbf6a9214398b28de3b0f668af3ca1
Alona Karpenko <alona.karpenko44@gmail.com>
2023-09-28 22:07:37 +0100

    house

---
+++ pageHouse/js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.materials__slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    
+  });
